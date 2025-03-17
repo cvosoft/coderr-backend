bf19fb2f55a84cb6b1747493968551826aae6959
Christoph Völker <christov@gmx.de>
2025-03-17 08:19:14 +0100

    test for base-view

--- base_info_app/tests.py
+++ base_info_app/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+class BaseViewTests(APITestCase):
+
+    def test_baseview(self):
+
+        url = reverse('base-info')
+
+        response = self.client.get(url, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        expected_keys = {"review_count", "average_rating",
+                         "business_profile_count", "offer_count", }
+        self.assertSetEqual(set(response.data.keys()), expected_keys)
