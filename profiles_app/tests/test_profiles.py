2292c4f4b08dfc5a61849f39dccf37ee109451b1
Christoph Völker <christov@gmx.de>
2025-03-18 20:35:48 +0100

    profile test

--- profiles_app/tests/test_profiles.py
+++ profiles_app/tests/test_profiles.py
@@ -9,8 +9,24 @@ from rest_framework.authtoken.models import Token
 
 class ProfileTests(APITestCase):
 
+    def setUp(self):
+        self.url = reverse('offer-list')
+        self.user = User.objects.create_user(
+            username="testuser", password="dsgdsggds"
+        )
+
+        self.user.userprofile.type = "customer"
+        self.user.userprofile.save()
+
+        # Token für den Benutzer erstellen
+        self.token = Token.objects.create(user=self.user)
+
+        # Authentifizierungs-Header setzen
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+
     def test_get_profile_success(self):
-        url = reverse('userprofile-detail')
+        url = reverse('userprofile-detail',
+                      kwargs={'user': self.user.id})
         response = self.client.get(url, format="json")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
