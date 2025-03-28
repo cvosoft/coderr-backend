41e2048ec3c282270f99558dd447554ef9bfd376
Christoph Völker <christov@gmx.de>
2025-03-28 14:43:26 +0100

    reviews test

--- reviews_app/api/views.py
+++ reviews_app/api/views.py
@@ -3,7 +3,8 @@ from .serializers import ReviewsSerializer, ReviewsPOSTSerializer
 from reviews_app.models import Reviews
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
+from rest_framework import filters, status
+from rest_framework.response import Response
 
 # class ReviewsDetailView(generics.RetrieveUpdateDestroyAPIView):
 #     queryset = Reviews.objects.all()
@@ -31,3 +32,11 @@ class ReviewViewSet(viewsets.ModelViewSet):
     # sorgt dafür dass reviewer id automatisch intern gespeichert wird
     def perform_create(self, serializer):
         serializer.save(reviewer=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        post_serializer = self.get_serializer(data=request.data)
+        post_serializer.is_valid(raise_exception=True)
+        instance = post_serializer.save(reviewer=self.request.user)
+
+        output_serializer = ReviewsSerializer(instance)
+        return Response(output_serializer.data, status=status.HTTP_201_CREATED)
--- reviews_app/tests/tests.py
+++ reviews_app/tests/tests.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
 
 # Create your tests here.
+class ReviewsTests(APITestCase):
+
+    def setUp(self):
+        url = reverse('registration')
+
+        data_busi = {
+            "username": "exampleUsername",
+            "email": "example@mail.de",
+            "password": "examplePassword",
+            "repeated_password": "examplePassword",
+            "type": "business"
+        }
+
+        data_cust = {
+            "username": "exampleUsername2",
+            "email": "example2@mail.de",
+            "password": "examplePassword",
+            "repeated_password": "examplePassword",
+            "type": "customer"
+        }
+
+        response1 = self.client.post(url, data_busi, format="json")
+        response2 = self.client.post(url, data_cust, format="json")
+
+        # variablen in self speichern, dann sind sie global verfügbar
+        self.token1 = response1.data['token']
+        self.user_id1 = response1.data['user_id']
+        self.token2 = response2.data['token']
+        self.user_id2 = response2.data['user_id']
+
+    def test_post_review(self):
+        # Authentifizierungs-Header setzen
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token2}')
+
+        data = {
+            "business_user": self.user_id1,
+            "rating": 4,
+            "description": "Alles war toll!"
+        }
+
+        url = reverse('reviews-list')
+
+        response = self.client.post(url, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Erwartete Keys
+        expected_keys = [
+            "id",
+            "business_user",
+            "reviewer",
+            "rating",
+            "description",
+            "created_at",
+            "updated_at"
+        ]
+
+        for key in expected_keys:
+            self.assertIn(key, response.data)
