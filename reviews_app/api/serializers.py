2206cae3de00560ff458f1314169f649025f4c80
Christoph Völker <christov@gmx.de>
2025-03-28 15:12:43 +0100

    permissions reviews

--- reviews_app/api/serializers.py
+++ reviews_app/api/serializers.py
@@ -16,3 +16,13 @@ class ReviewsPOSTSerializer(serializers.ModelSerializer):
         model = Reviews
         # nur drei felder rausschicken
         fields = ["business_user", "rating", "description"]
+
+    def validate(self, data):
+        request = self.context['request']
+        reviewer = request.user
+        business_user = data.get('business_user')
+
+        if Reviews.objects.filter(reviewer=reviewer, business_user=business_user).exists():
+            raise serializers.ValidationError(
+                "Du hast diesen Anbieter bereits bewertet.")
+        return data
--- reviews_app/models.py
+++ reviews_app/models.py
@@ -5,6 +5,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Reviews(models.Model):
 
+    # jeder reviewer darf nur jeden business nur einmal bewerten!
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['reviewer', 'business_user'],
+                name='unique_review_per_user_pair'
+            )
+        ]
+
     reviewer = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="reviews_given")
     business_user = models.ForeignKey(
